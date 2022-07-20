52b90bc825c66978b269001cf39524b4da35b6ec
Your Name~ <you@example.com>
2022-07-20 19:36:52 +0100

    navbar with logo working

--- video-sharing/component/Navbar.tsx
+++ video-sharing/component/Navbar.tsx
@@ -4,10 +4,26 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { AiOutlineLogout } from 'reac-icons';
-
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/utils/vidShare.png'
 function navbar() {
   return (
-    <div>navbar</div>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[130px]'>
+          <Image 
+            className='cursor-pointer'
+            src={Logo}
+            alt="vidshare"
+            layout='responsive'
+          
+          
+          />
+        </div>
+      
+      </Link>
+    </div>
   )
 }
 
